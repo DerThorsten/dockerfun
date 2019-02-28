c74e2db6c11b7a7e357fec1d6eea3e34aa8f80a7
DerThorsten <derthorstenbeier@gmail.com>
2019-02-28 13:19:01 +0100

    changed dockerfile

--- dockerfun/run_all.sh
+++ dockerfun/run_all.sh
@@ -1,4 +1,4 @@
- sudo sudo docker run --runtime=nvidia --rm fresh  /bin/bash -c "	
+ sudo sudo docker run --runtime=nvidia --rm moredeps  /bin/bash -c "	
 	kipoi get-example DeepSEA/predict -o example && \
 	kipoi predict DeepSEA/predict \
  	--dataloader_args='{\"intervals_file\": \"example/intervals_file\", \"fasta_file\": \"example/fasta_file\"}' \
